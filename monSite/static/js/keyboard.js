4cb1b7c5fdfd3527aaac745c23c997dde7db210e
charles-dubos <49372663+charles-dubos@users.noreply.github.com>
2024-02-12 20:40:15 +0100

    Update keyboard.js
    Ajout mouvements android

--- monSite/static/js/keyboard.js
+++ monSite/static/js/keyboard.js
@@ -62,3 +62,47 @@ document.onkeydown = function(e) {
     }
   }
 }
+
+// Gestion des mouvements Android
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+                                                                         
+function handleTouchStart(evt) {
+  const firstTouch = evt.touches[0];                                      
+  xDown = firstTouch.clientX;                                      
+  yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;                                    
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+                                                                         
+  if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+    if ( xDiff > 0 ) {
+      /* right swipe */ 
+      linkTo( null, document.getElementById('kbdPageDown'));
+    } else {
+      /* left swipe */
+      linkTo( null, document.getElementById('kbdPageUp'));
+    }                       
+  } else {
+    if ( yDiff > 0 ) {
+      /* down swipe */
+    } else { 
+      /* up swipe */
+    }                                                                 
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;                                             
+};
