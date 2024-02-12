513341c19c6f4e32db6dd6360bbba05c13cc8ccd
charles-dubos <49372663+charles-dubos@users.noreply.github.com>
2024-02-12 10:02:12 +0100

    Add keyboard.js
    Prise en compte des touches clavier
    - Numérotées
    - Enter
    - Back
    - Escape
    Avec temporisation 3s

---
+++ monSite/static/js/keyboard.js
@@ -0,0 +1,58 @@
+const TIME_OUT = 3000; // Timeout par défaut en millisecondes
+let selectedChannel = "";
+
+function linkTo( e, dom ) {
+  if ( dom.nodeName == "A" ) {
+    window.location.href = dom.href;
+  } else if ( dom.nodeName == "BUTTON" ) {
+    dom.submit()
+  }
+  if ( e ) e.preventDefault();
+}
+
+document.onkeydown = function(e) {
+  console.log(e.key)
+  if ( isNaN(e.key) ) {
+    domKeyboardElements = document.querySelectorAll('[id^=kbd'+e.key+']');
+    if ( domKeyboardElements.length !== 0 ) {
+      linkTo(e, domKeyboardElements[0]);
+    } else  {
+      switch (e.key) {
+
+        case 'Enter':
+          // Va vers le lien correspondant au chiffre tapé
+          domKeyboardElement = document.getElementById('kbd'+selectedChannel);
+          if ( domKeyboardElement ) {
+            linkTo(e, domKeyboardElement);
+          }
+          break;
+
+          case 'Backspace':
+            // Retour arrière, enlève le dernier caractère
+            selectedChannel = selectedChannel.slice(0, -1);
+            document.getElementById('selectedChannel').innerHTML = selectedChannel;
+            break;
+
+            case 'Escape':
+              // Echap, quitte la fenêtre
+              window.close();
+              break;
+        }
+    }
+  } else {
+    // Ne pas prendre en compte le cas du 0 si vide
+    if ( e.key != '0' || selectedChannel.length !=0 ) {
+      selectedChannel = selectedChannel + e.key;
+      document.getElementById('selectedChannel').innerHTML = selectedChannel;
+
+      // Timer d'auto-validation
+      window.clearTimeout()
+      window.setTimeout(() => {
+        domKeyboardElement = document.getElementById('kbd'+selectedChannel);
+        if ( domKeyboardElement ) {
+          linkTo(null, domKeyboardElement);
+        }
+      }, TIME_OUT);
+    }
+  }
+}
