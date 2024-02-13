559e24bb82e9d866f24bae1f1f9ecdebeb97dbdd
charles-dubos <49372663+charles-dubos@users.noreply.github.com>
2024-02-13 18:46:57 +0100

    No cache

--- monSite/pages.py
+++ monSite/pages.py
@@ -190,3 +190,16 @@ def edit(path):
                            newNumber=firstAvailable(database=database, current=path),
                            message=message
                            )
+
+@bp.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+    
