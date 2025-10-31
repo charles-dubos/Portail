69e67282b1964f9b8d8420918dc123adeee4273b
charles-dubos <49372663+charles-dubos@users.noreply.github.com>
2025-10-31 21:33:05 +0100

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,8 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-threads = int(os.environ.get('GUNICORN_THREADS', '4'))
-timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
+threads = int(os.environ.get('GUNICORN_THREADS', '2'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '60'))
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8000')
 
