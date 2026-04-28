f9ec85ea07bfe09d8a8b6c4757bbbe08a3ba2ffe
Papa Dubs <Papa@Dubs>
2026-04-28 22:33:16 +0200

    changing log stream to stderr

--- portail/functions/general.py
+++ portail/functions/general.py
@@ -10,7 +10,7 @@ def loadLogging(logConfig: dict) -> None:
   """
   logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    stream=sys.stdout,
+    stream=sys.stderr,
     level=getattr( logging, logConfig['LOGLEVEL'] )
   )
 
