1697b2da9b760f645d8ba1b92491ae527987d670
Papa Dubs <Papa@Dubs>
2026-05-09 12:28:11 +0200

    Correction erreur bouton darkmode

--- portail/static/js/global.js
+++ portail/static/js/global.js
@@ -26,16 +26,18 @@ const appMuted = {
 const darkMode = {
   state() { return getCookie('darkmode') },
   updateButton() {
-    document.getElementById("toggleDarkmode").children[0].classList.remove('fa-moon','fa-sun', 'fa-circle-half-stroke');
-    switch (this.state()) {
-      case "true":
-        document.getElementById("toggleDarkmode").children[0].classList.add('fa-moon');
-        break;
-      case "false":
-        document.getElementById("toggleDarkmode").children[0].classList.add('fa-sun');
-        break;
-      default:
-        document.getElementById("toggleDarkmode").children[0].classList.add('fa-circle-half-stroke');
+    if (document.getElementById("toggleDarkmode") != null) {
+      document.getElementById("toggleDarkmode").children[0].classList.remove('fa-moon','fa-sun', 'fa-circle-half-stroke');
+      switch (this.state()) {
+        case "true":
+          document.getElementById("toggleDarkmode").children[0].classList.add('fa-moon');
+          break;
+        case "false":
+          document.getElementById("toggleDarkmode").children[0].classList.add('fa-sun');
+          break;
+        default:
+          document.getElementById("toggleDarkmode").children[0].classList.add('fa-circle-half-stroke');
+      }
     }
   },
   switch() {
